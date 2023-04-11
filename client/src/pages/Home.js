8a563b88899893c212594aa8d709381d41a071a6
Manuel Castro <manuelcastro2021@fau.edu>
2023-04-10 22:25:45 -0400

    Small change in ui

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,6 +8,9 @@ function Home () {
             <h1 className='display-1 text-success text-center'>
             Welcome to student Registry APP
             </h1>
+            <h1 className='display-3 text-danger text-center'>
+            Version 2.0 multideployment
+            </h1>
             <Alert variant={'success'} className='display-4 text-center my-5'>React Version</Alert>
             {/* <h5 className='display-5 text-dark text-center pt-3text-center'>React Version</h5> */}
             <h3 className='display-5 text-primary text-center '>Manuel Castro</h3>
