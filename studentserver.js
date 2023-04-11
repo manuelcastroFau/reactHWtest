674b886333fcf5181f78c699ef20047b909353e7
Manuel Castro <manuelcastro2021@fau.edu>
2023-04-10 20:38:02 -0400

    x

--- studentserver.js
+++ studentserver.js
@@ -81,7 +81,7 @@ app.get('*-Student*', (req, res) => {
 });
 
 app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, 'build','index.html'));
+  res.sendFile(path.join(__dirname, 'client/build','client/index.html'));
 })
 
 /**
